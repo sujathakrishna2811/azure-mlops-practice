0830d5c521a1acb00f03cf1d0a96156e2f35fcf9
sujathakrishna2811 <58307187+sujathakrishna2811@users.noreply.github.com>
2026-05-07 12:44:28 -0400

    Update register_best_model.py

--- scripts/register_best_model.py
+++ scripts/register_best_model.py
@@ -1,24 +1,27 @@
 
 from azure.ai.ml.entities import Model
 from azure.ai.ml.constants import AssetTypes
-
 import argparse
+from azure.ai.ml import MLClient
+from azure.identity import DefaultAzureCredential
+import os
+
+ml_client = MLClient(
+    DefaultAzureCredential(),
+    subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
+    resource_group_name=os.environ["AZURE_RESOURCE_GROUP"],
+    workspace_name=os.environ["AZURE_ML_WORKSPACE"]
+)
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("--sweep_job_name", required=True)
-
 args = parser.parse_args()
-
 sweep_job_name = args.sweep_job_name
-
 job = ml_client.jobs.get(sweep_job_name)
-
 
 best_job_name = job.properties["best_child_run_id"]
 
 print("Best job:", best_job_name)
-
 
 #Register BEST model (automatic)
 model = Model(
