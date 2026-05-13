82e0f4deb9e743fc5f0aa12f72d4917e0f7f4e25
sujathakrishna2811 <58307187+sujathakrishna2811@users.noreply.github.com>
2026-05-13 12:54:11 -0400

    Update model_deployment_script.py

--- titanic-survival/scripts/model_deployment_script.py
+++ titanic-survival/scripts/model_deployment_script.py
@@ -55,7 +55,7 @@ deployment = ManagedOnlineDeployment(
         code="titanic-survival/src/",
         scoring_script="score.py",
     ),
-    instance_type="Standard_DS3_v2",
+    instance_type="Standard_D2AS_v4",
     instance_count=1,
 )
 
